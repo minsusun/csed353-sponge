c4b3eb245cc3efb0c3e59dc98df38718a25ea844
minsusun <poodding397@gmail.com>
2024-03-10 18:42:53 +0900

    assn1: reassembler implementation
    + use iterator on string
    + correct eof handling
    + correct _assembled_iterator
    + correct redudant data

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -13,9 +13,9 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 inline void StreamReassembler::_fetchAssembled() {
+    for (_assembled_iterator = _buffer.begin(); _assembled_iterator != _buffer.end() && _assembled_cursor > (*_assembled_iterator).first; _assembled_iterator++);
     for (; _assembled_iterator != _buffer.end() && _assembled_cursor == (*_assembled_iterator).first;
-         _assembled_iterator++, _assembled_cursor++)
-        _unassembled_bytes--;
+         _assembled_iterator++, _assembled_cursor++, _unassembled_bytes--);
 }
 
 inline void StreamReassembler::_pushAssembled() {
@@ -23,6 +23,21 @@ inline void StreamReassembler::_pushAssembled() {
         if (_output.buffer_size() >= _capacity)
             break;
         else {
+            cout << "!" << (*it).first << " " << (*it).second << endl;
+            _output.write((*it).second);
+            it = _buffer.erase(it);
+            _buffer_size--;
+            break;
+        }
+    }
+}
+
+inline void StreamReassembler::_pushAssembledAll() {
+    for (auto it = _buffer.begin(); it != _assembled_iterator;) {
+        if (_output.buffer_size() >= _capacity)
+            break;
+        else {
+            cout << "!" << (*it).first << " " << (*it).second << endl;
             _output.write((*it).second);
             it = _buffer.erase(it);
             _buffer_size--;
@@ -44,36 +59,63 @@ StreamReassembler::StreamReassembler(const size_t capacity)
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const uint64_t index, const bool eof) {
+    cout << data.size() << " " << index << " " << eof << endl;
+    cout << _assembled_cursor << endl;
+    cout << _eof << endl;
+    
+    if(data.empty() && eof) _eof = true;
+
     uint64_t pos = index;
-    for (auto &elem : data) {
-        // ignore already assembled bytes
-        if (pos < _assembled_cursor) {
+    for (auto it = data.begin(); it != data.end(); it++) {
+        // try to push assembled data
+        // _pushAssembled();
+
+        if (pos < _output.bytes_written()) {
             pos++;
             continue;
         }
 
-        // try to push assembled data
-        _pushAssembled();
+        if (_buffer.find(pos) == _buffer.end()) {
+            if (_buffer_size >= _capacity) break;
 
-        // buffer is full
-        if (_buffer_size >= _capacity)
-            break;
+            if (_buffer_size == _capacity - 1 && pos != _assembled_cursor) break;
 
-        // map.insert.second will be true when new data is inserted, false for duplicated data
-        if (_buffer.insert({pos++, to_string(elem)}).second)
             _buffer_size++, _unassembled_bytes++;
+        }
+        
+        if(next(it) == data.end() && eof) _eof = true;
+
+        _buffer[pos++] = string(it, next(it));
+
+        // when buffer is full
+        // if (_buffer_size >= _capacity) break;
+
+        // if (_buffer_size == _capacity - 1 && pos != _assembled_cursor) break; 
+
+        // if(next(it) == data.end() && eof) _eof = true;
+
+        // // ignore already assembled bytes
+        // if (pos < _assembled_cursor) {
+        //     if(_buffer.find(pos) != _buffer.end()) _buffer[pos] = string(it, next(it));
+        //     pos++;
+        //     continue;
+        // }
+            
+        // // index not existed
+        // if (_buffer.find(pos) == _buffer.end()) _buffer_size++, _unassembled_bytes++;
+        // _buffer[pos++] = string(it, next(it));
 
         // fetch assembled iterator to new assembled data and try to push assembled data
         _fetchAssembled();
         _pushAssembled();
-
-        // fetch eof value when loop reach the end of data
-        if (elem == data[data.size() - 1])
-            _eof = eof;
     }
+
+    _pushAssembledAll();
 
     if (_eof && empty())
         _output.end_input();
+    cout << _buffer_size << endl;
+    cout << _output.input_ended() << endl;
 }
 
 size_t StreamReassembler::unassembled_bytes() const { return _unassembled_bytes; }

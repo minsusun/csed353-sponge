62c1fb7f307dd7678acd2c0bf002ea6c79f261b2
minsusun <poodding397@gmail.com>
2024-03-22 13:19:44 +0900

    assn2: WrappingInt32 comments

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -26,15 +26,21 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32(isn + n
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
+    // Retrieve lower 32 bits of total 64 bits. Automatic rounding.
     const uint64_t lower_32 = n.raw_value() - isn.raw_value();
+    // Retrieve upper 32 bits of total 64 bits.
+    // By now (upper_32 << 32) | lower_32 < checkpoint holds unless upper_32 is 0xFFFFFFFF
     const uint64_t upper_32 = (checkpoint - lower_32) >> 32;
 
+    // Edge case
     if (checkpoint <= lower_32)
         return lower_32;
 
+    // Candidates for unwrapped result
     const uint64_t low = (upper_32 << 32) | lower_32;
     const uint64_t high = ((upper_32 + 1) << 32) | lower_32;
 
+    // Determine which candidate to return
     if (checkpoint - low >= high - checkpoint)
         return high;
     else
